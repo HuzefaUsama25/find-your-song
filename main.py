9c592a87bc92d087c0df4538dd5e2e7301c90f71
HuzefaUsama25 <huzefausama25@gmail.com>
2021-08-15 11:56:17 +0500

    Added Voice Recognition

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as bs4
+import webbrowser
+import speech_recognition as sr
+import time
+
 
 USER_AGENT = UserAgent()
-print(USER_AGENT.random)
+HTML_PARSER = "html.parser"
+SPEECH_RECOGNIZER = sr.Recognizer()
+
+
+def listen_to_lyrics():
+    with sr.Microphone() as source:
+        print("Listening..")
+        lyrics_audio = SPEECH_RECOGNIZER.listen(source)
+        print("Processing..")
+
+        try:
+            lyrics_audio_string = SPEECH_RECOGNIZER.recognize_google(
+                lyrics_audio)
+            print("Lyrics: " + lyrics_audio_string)
+            return lyrics_audio_string
+        except:
+            print("Sorry, I did not get that")
+            lyrics_string = input("Lyrics: ")
+            return lyrics_string
 
 
 def song_info_from_lyrics(lyrics):
@@ -18,7 +40,7 @@ def song_info_from_lyrics(lyrics):
     request = requests.get(search_url, headers=headers)
 
     if request.status_code == 200:
-        soup = bs4(request.content, 'html.parser')
+        soup = bs4(request.content, HTML_PARSER)
 
     song_info_div_heads = soup.find_all(class_="head")
 
@@ -56,6 +78,60 @@ def user_song_of_choice(song_links):
             print("\nenter the index of song you want to listen")
             continue
 
-    user_song_url = f"https://songsear.ch/q{user_song_url_raw}"
+    user_song_url = f"https://songsear.ch{user_song_url_raw}"
 
     return user_song_url
+
+
+def get_complete_lyrics(lyrics_url):
+    headers = {
+        "User-Agent": USER_AGENT.random,
+    }
+
+    request = requests.get(lyrics_url, headers=headers)
+
+    if request.status_code == 200:
+        soup = bs4(request.content, HTML_PARSER)
+
+    song_link = request.url
+    info_song_link = song_link.split("/")
+    song_artist = info_song_link[4].replace("-", " ")
+    song_name = info_song_link[5].replace("-", " ")
+
+    lyrics_blockquote = soup.find("blockquote")
+    lyrics_blockquote_string = str(lyrics_blockquote)
+    negatives = ["<blockquote>", "</blockquote>", "<br/>"]
+    for negative in negatives:
+        lyrics_blockquote_string = lyrics_blockquote_string.replace(
+            negative, "\n")
+
+    lyrics = lyrics_blockquote_string
+
+    # write lyrics to file!
+    with open(f"{song_name.upper()} - {song_artist}.txt", "wb") as file:
+        file.write(lyrics.encode("utf-8"))
+
+    verses = lyrics.split("\n")
+    for verse in verses:
+        print(verse)
+        time.sleep(0.5)
+
+
+def play_on_youtube(song_link):
+    info_song_link = song_link.split("/")
+    song_artist = info_song_link[4].replace("-", " ")
+    song_name = info_song_link[5].replace("-", " ")
+    webbrowser.open_new(
+        f"https://www.youtube.com/results?search_query={song_name}+by+{song_artist}")
+
+
+def main():
+    user_lyrics = listen_to_lyrics()
+    song_links = song_info_from_lyrics(user_lyrics)
+    user_choosed_song = user_song_of_choice(song_links)
+    get_complete_lyrics(user_choosed_song)
+    play_on_youtube(user_choosed_song)
+
+
+if __name__ == '__main__':
+    main()
